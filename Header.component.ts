386bddeea1d980be915f029376e7eed194ed80ee
Kanthravi55 <38487052+Kanthravi55@users.noreply.github.com>
2020-02-28 23:21:47 +0530

    Add files via upload

---
+++ Header.component.ts
@@ -0,0 +1,15 @@
+import { Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './Header.component.html'
+})
+
+export class HeaderComponent {
+// @Output() featureSelected = new EventEmitter<string>();  (using routerLink)
+ // onSelect(feature: string) {
+//    this.featureSelected.emit(feature);   (using routerLink)
+//  }
+}
+
